d0ef41aff204791f0eb57758ca1ad860032ca2fd
Jagdish Salgotra <jagdishsal@gmail.com>
2019-12-12 22:31:22 +0530

    Add Expensify styles

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = (env) => {
    const CSSExtract = new ExtractTextPlugin('styles.css');
 
    return {
-      entry: './src/app.js',
+      entry: ['babel-polyfill', './src/app.js'],
       output: {
          path: path.join(__dirname, 'public', 'dist'),
          filename: 'bundle.js'
@@ -63,38 +63,6 @@ module.exports = (env) => {
             'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
             'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
             'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-         }),
-         new HardSourceWebpackPlugin({
-            // Either an absolute path or relative to webpack's options.context.
-            cacheDirectory: 'node_modules/.cache/hard-source/[confighash]',
-            // Either a string of object hash function given a webpack config.
-            configHash: function(webpackConfig) {
-               // node-object-hash on npm can be used to build this.
-               return require('node-object-hash')({sort: false}).hash(webpackConfig);
-            },
-            // Either false, a string, an object, or a project hashing function.
-            environmentHash: {
-               root: process.cwd(),
-               directories: [],
-               files: ['package-lock.json', 'yarn.lock'],
-            },
-            // An object.
-            info: {
-               // 'none' or 'test'.
-               mode: 'none',
-               // 'debug', 'log', 'info', 'warn', or 'error'.
-               level: 'debug',
-            },
-            // Clean up large, old caches automatically.
-            cachePrune: {
-               // Caches younger than `maxAge` are not considered for deletion. They must
-               // be at least this (default: 2 days) old in milliseconds.
-               maxAge: 2 * 24 * 60 * 60 * 1000,
-               // All caches together must be larger than `sizeThreshold` before any
-               // caches will be deleted. Together they must be at least this
-               // (default: 50 MB) big in bytes.
-               sizeThreshold: 50 * 1024 * 1024
-            },
          })
       ]
    };
